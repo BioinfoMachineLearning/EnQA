d3f4c4bb8afdab04861b512d45895144833ccad7
Natalia Zenkova <natalia.v.zenkova@gmail.com>
2022-08-10 13:00:14 +0000

    add comments, rename output files

--- process.py
+++ process.py
@@ -1,6 +1,6 @@
 import os
-import pickle
-import shutil
+import logging
+from re import I
 
 import torch
 import argparse
@@ -13,40 +13,58 @@ from feature import create_basic_features, get_base2d_feature
 from data.process_alphafold import process_alphafold_target_ensemble, process_alphafold_model, mergePDB, process_without_af_model
 
 
-def process(input_str: str, output_str: str, 
-            alphafold_prediction: str='', label_pdb: str='', name: str='') -> None:
+def process(
+    input_pdb: str, 
+    output_path: str,
+    alphafold_prediction: str='', 
+    label_pdb: str='', 
+    input_name: str=''
+) -> None:
+    """
+    Generate the labels and features for training
+    without the predicted results from AlphaFold and with the corresponding native PDBs.
+    @param input_pdb: path to input pdb file
+    @param output_path: path to output folder
+    @param alphafold_prediction: path to alphafold prediction results
+    @param label_pdb: path to reference pdb file
+    @param input_name: structure id
+    @return: None
+    """
     
     device = 'cpu'
     lddt_cmd = 'utils/lddt'
-    if not os.path.isdir(output_str):
-        os.makedirs(output_str)
+    if not os.path.isdir(output_path):
+        os.makedirs(output_path)
 
     # Featureize
     disto_types = ['esto9']
-    input_name = os.path.basename(input_str).replace('.pdb', '')
-    ppdb = PandasPdb().read_pdb(input_str)
+    # input_name = os.path.basename(input_pdb).replace('.pdb', '')
+    ppdb = PandasPdb().read_pdb(input_pdb)
     is_multi_chain = len(ppdb.df['ATOM']['chain_id'].unique()) > 1
-    temp_dir = output_str+ '/tmp/'
+    temp_dir = output_path + '/tmp/'
 
     if is_multi_chain:
         if not os.path.isdir(temp_dir):
             os.makedirs(temp_dir)
         outputPDB = os.path.join(temp_dir, 'merged_' + input_name + '.pdb')
-        mergePDB(input_str, outputPDB, newStart=1)
-        input_str = outputPDB
-
-    one_hot, features, pos, sh_adj, el = create_basic_features(input_str, output_str, template_path=None,
+        mergePDB(input_pdb, outputPDB, newStart=1)
+        input_pdb = outputPDB
+    
+    # Generate the features
+    # create one_hot, features (volume, buriedness, sasa), coordinates for c_alpha,
+    # spherical embeddings, coordinates for close c_alpha
+    one_hot, features, pos, sh_adj, el = create_basic_features(input_pdb, output_path, template_path=None,
                                                                diff_cutoff=15, coordinate_factor=0.01)
     sh_data = expand_sh(sh_adj, one_hot.shape[1])
     
     if not alphafold_prediction:
-        af2_qa, plddt, cmap, dict_2d = process_without_af_model(input_str)
+        af2_qa, plddt, cmap, dict_2d = process_without_af_model(input_pdb)
     else:
-        af2_qa = process_alphafold_model(input_str, alphafold_prediction, lddt_cmd, n_models=5,
+        af2_qa = process_alphafold_model(input_pdb, alphafold_prediction, lddt_cmd, n_models=5,
                                      is_multi_chain=is_multi_chain, temp_dir=temp_dir)
         plddt, cmap, dict_2d = process_alphafold_target_ensemble(alphafold_prediction, disto_types,
                                                              n_models=5, cmap_cutoff_dim=42,
-                                                       input_pdb_file=input_str)
+                                                       input_pdb_file=input_pdb)
 
     f2d = np.concatenate((sh_data, dict_2d['esto9']), axis=0)
     f1d = np.concatenate((one_hot, features, plddt, af2_qa), axis=0)
@@ -56,22 +74,18 @@ def process(input_str: str, output_str: str,
     el = [torch.tensor(i).to(device) for i in el]
     cmap = torch.tensor(cmap).to(device)
 
-    label_lddt = generate_lddt_score(input_str, label_pdb, 'utils/lddt') # WHAT?
-    diff_bins = generate_dist_diff(input_str, label_pdb)
-    pos_transformed = generate_coords_transform(input_str, label_pdb, None)
+    # Generate the labels
+    label_lddt = generate_lddt_score(input_pdb, label_pdb, 'utils/lddt')
+    diff_bins = generate_dist_diff(input_pdb, label_pdb)
+    pos_transformed = generate_coords_transform(input_pdb, label_pdb, None)
     label_lddt = torch.tensor(label_lddt).to(device)
     diff_bins = torch.tensor(diff_bins).unsqueeze(0).to(device)
     pos_transformed = torch.tensor(pos_transformed/100).to(device)
 
-    if not name:
-        torch.save({"f1d": f1d, "f2d": f2d, "pos": pos, 'el': el, 'cmap': cmap,
+    torch.save({"f1d": f1d, "f2d": f2d, "pos": pos, 'el': el, 'cmap': cmap,
                 'label_lddt': label_lddt, 'diff_bins': diff_bins, 'pos_transformed': pos_transformed},
-               os.path.join(output_str, os.path.basename(input_str).replace('.pdb', '') + '.pt'))
-    else:
-        torch.save({"f1d": f1d, "f2d": f2d, "pos": pos, 'el': el, 'cmap': cmap,
-                'label_lddt': label_lddt, 'diff_bins': diff_bins, 'pos_transformed': pos_transformed},
-               os.path.join(output_str, f"{name}_{os.path.basename(input_str).replace('.pdb', '')}" + '.pt'))
-    
+               os.path.join(output_path, input_name + '.pt'))
+   
 
 # python3 process.py --input example/model/6KYTP/test_model.pdb --label_pdb example/model/6KYTP/test_model.pdb --output outputs/processed --alphafold_prediction example/alphafold_prediction/6KYTP/
 
